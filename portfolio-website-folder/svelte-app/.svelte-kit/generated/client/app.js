1a319e3ee9a324798711ab1b3bdf588419a845ad
Solomiya-Datskiv <c00301892@setu.ie>
2025-05-07 02:00:08 +0100

    build failed

--- portfolio-website-folder/svelte-app/.svelte-kit/generated/client/app.js
+++ portfolio-website-folder/svelte-app/.svelte-kit/generated/client/app.js
@@ -2,28 +2,13 @@ export { matchers } from './matchers.js';
 
 export const nodes = [
 	() => import('./nodes/0'),
-	() => import('./nodes/1'),
-	() => import('./nodes/2'),
-	() => import('./nodes/3'),
-	() => import('./nodes/4'),
-	() => import('./nodes/5'),
-	() => import('./nodes/6'),
-	() => import('./nodes/7'),
-	() => import('./nodes/8'),
-	() => import('./nodes/9')
+	() => import('./nodes/1')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [2],
-		"/about": [3],
-		"/contact": [4],
-		"/digitalart": [5],
-		"/drawing": [6],
-		"/otherprojects": [7],
-		"/painting": [8],
-		"/works": [9]
+		
 	};
 
 export const hooks = {
